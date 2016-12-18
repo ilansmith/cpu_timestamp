ec37a90641a60742cf15f49bd4e1d445e5fb0126
Ilan Smith <lunnys@gmail.com>
2016-12-19 00:51:23 +0200

    Added prefetching to enhance performance
    Signed-off-by: Ilan Smith <lunnys@gmail.com>

--- rdtsc.c
+++ rdtsc.c
@@ -3,6 +3,13 @@
 #include <stdio.h>
 #include <pthread.h>
 
+/**
+ * Cache Line Prefetch - Arch specific!
+ */
+#ifndef L1_CACHE_BYTES
+#define L1_CACHE_BYTES		64
+#endif
+
 /* Read time stamp couter */
 static void set_cpu_affinity(int cpu)
 {
@@ -26,6 +33,17 @@ static inline void gettimeoftsc(unsigned long long *tscval)
 	*tscval = (((unsigned long long)upper_32) << 32) | lower_32;
 }
 
+static inline void prefetch(void *x)
+{
+#if defined __i386__ || defined __x86_64__
+	asm volatile("prefetcht0 %0" :: "m" (*(unsigned long *)x));
+#else
+	/* Use simple memcpy to get data into cache */
+	char temp_prefetch_block[L1_CACHE_BYTES];
+	memcpy(temp_prefetch_block, x, L1_CACHE_BYTES);
+#endif
+}
+
 int main(int argc, char **argv)
 {
 	unsigned long long start = 0, end = 0;
@@ -38,6 +56,8 @@ int main(int argc, char **argv)
 
 	set_cpu_affinity(0);
 
+	prefetch(&set_cpu_affinity);
+	prefetch(&gettimeoftsc);
 	x = (unsigned long)&gettimeoftsc - (unsigned long)&set_cpu_affinity;
 
 	gettimeoftsc(&start);
